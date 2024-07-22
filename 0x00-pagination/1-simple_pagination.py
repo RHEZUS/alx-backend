24b7a86129e186f4d619afa75d361582ccf630b1
RHEZUS <ludivinseunda2@gmail.com>
2024-07-22 12:52:06 +0300

    solve all taska

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -32,8 +32,8 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
+        assert type(page) == int and page > 0
+        assert type(page_size) == int and page_size > 0
         start, end = index_range(page, page_size)
         data = self.dataset()
         if start >= len(data):
