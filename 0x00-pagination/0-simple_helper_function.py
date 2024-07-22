8f1892d8bbd52159875d6f7aa36624d3b3638825
RHEZUS <ludivinseunda2@gmail.com>
2024-07-22 12:43:34 +0300

    solve all taska

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,6 +4,8 @@ from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """ Returns a tuple of size two containing
-    a start index and an end index """
-    return ((page - 1) * page_size, page * page_size)
+    """Retrieves the index range from a given page and page size.
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -5,9 +5,11 @@ from typing import List, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """ Returns a tuple of size two containing
-    a start index and an end index """
-    return ((page - 1) * page_size, page * page_size)
+    """Retrieves the index range from a given page and page size.
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
 
 
 class Server:
